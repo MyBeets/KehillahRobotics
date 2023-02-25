0ac10e02f9d3147da0cc0a73532e8c0a94d66288
henri849 <hsayag2024@kehillahstudent.org>
2023-02-24 16:38:15 -0800

    added final conversions to angles and tester code

--- Variables.py
+++ Variables.py
@@ -41,6 +41,27 @@ class Angle(Variable):
    180---0
        |270
     """
+    def data(self):
+        if self.type == 1:
+            return self.calc2data()
+        if self.type == 2:
+            return self.display2data()
+        return self.value
+    
+    def calc(self):
+        if self.type == 0:
+            return self.data2calc()
+        if self.type == 2:
+            return self.display2calc()
+        return self.value
+    
+    def display(self):
+        if self.type == 0:
+            return self.data2display()
+        if self.type == 1:
+            return self.calc2display()
+        return self.value
+
     def display2data(self):
         if self.value <= 180:
             return self.value -90
@@ -48,7 +69,7 @@ class Angle(Variable):
             return 180-(self.value -90)%180
     def display2calc(self):
         return 180 - self.value
-    def data2Display(self):
+    def data2display(self):
         return self.value + 90
     def data2calc(self): # this shouldn't happen too much as there would be a loss of information
         return 90- self.value 
@@ -56,8 +77,6 @@ class Angle(Variable):
         return -self.value +180
     def calc2data(self):
         return abs(90-self.value)
-
-
 
 class Force(Variable):
     pass
@@ -73,12 +92,31 @@ if __name__ == "__main__":
     #Basic Variable Class:
     basicVar = Variable(1,10)#calc
     pas+= basicVar.data() == 10;fail+= basicVar.data() != 10
+    pas+= basicVar.calc() == 10;fail+= basicVar.calc() != 10
+    pas+= basicVar.display() == 10;fail+= basicVar.display() != 10
     pas+= str(basicVar) == "Calc: 10";fail+= str(basicVar) != "Calc: 10"
 
     #Basic Angle Class:
-    angleVar = Angle(1, 40) # calc
-    pas+= str(angleVar) == "Calc: 40";fail+= str(angleVar) != "Calc: 40"
-    pas+= angleVar.calc2display() == 140;fail+= angleVar.calc2display() != 140
-    pas+= Angle(1, 270).calc2data() == 180;fail+= Angle(1, 270).calc2data() != 180
+    calcAngle = Angle(1, 40) # calc
+    pas+= str(calcAngle) == "Calc: 40";fail+= str(calcAngle) != "Calc: 40"
+    pas+= calcAngle.calc2display() == 140;fail+= calcAngle.calc2display() != 140
+    pas+= calcAngle.display() == 140;fail+= calcAngle.display() != 140
+    pas+= calcAngle.calc2data() == 50;fail+= calcAngle.calc2data() != 50
+    pas+= calcAngle.data() == 50;fail+= calcAngle.data() != 50
+    pas+= calcAngle.calc() == 40;fail+= calcAngle.calc() != 40
+
+    dataAngle = Angle(0, 90)#data
+    pas+= dataAngle.data2calc() == 0;fail+= dataAngle.data2calc() != 0
+    pas+= dataAngle.calc() == 0;fail+= dataAngle.calc() != 0
+    pas+= dataAngle.data2display() == 180;fail+= dataAngle.data2display() != 180
+    pas+= dataAngle.display() == 180;fail+= dataAngle.display() != 180
+    pas+= dataAngle.data() == 90;fail+= dataAngle.data() != 90
+
+    displayAngle = Angle(2, 0)#display
+    pas+= displayAngle.display2calc() == 180;fail+= displayAngle.display2calc() != 180
+    pas+= displayAngle.calc() == 180;fail+= displayAngle.calc() != 180
+    pas+= displayAngle.display2data() == -90;fail+= displayAngle.display2data() != -90
+    pas+= displayAngle.data() == -90;fail+= displayAngle.data() != -90
+    pas+= displayAngle.display() == 0;fail+= displayAngle.display() != 0
 
     print("passed: " + str(pas) + ", failed: " + str(fail)+ ", of: " +str(pas+fail))

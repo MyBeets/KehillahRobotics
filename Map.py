d314fed309a4d117d473fa1a5c90a94816ac8839
henri849 <hsayag2024@kehillahstudent.org>
2023-03-11 13:34:45 -0800

    added copyright notice and shoreline's adress

--- Map.py
+++ Map.py
@@ -2,10 +2,10 @@ import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-place = "Lake Attitash, Amesbury, Essex County, Massachusetts, USA"
-
-area = ox.geocode_to_gdf(place)
-
+lakeAttitash = "Lake Attitash, Amesbury, Essex County, Massachusetts, USA"
+lakeShoreline = "Shoreline lake, Mountain View, Santa Clara County, California, United States"
+area = ox.geocode_to_gdf(lakeAttitash)
 area.plot()
+plt.figtext(0, 0.1, '© OpenStreetMap contributors', fontsize = 10)
 plt.show()
-print(area)
+#print(area)

e5c18a81f7fe0cee0515550747c864c08f6bb5b6
henri849 <hsayag2024@kehillahstudent.org>
2023-03-11 22:13:21 -0800

    turned display methods into class and started experimenting with sliders

--- Display.py
+++ Display.py
@@ -1,27 +1,66 @@
+#Display and Map
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+from matplotlib.widgets import Slider, Button
 from Map import regionPolygon
+#Boat and variables
 from Foil import foil
+from Variables import *
+from Boat import Boat
+#Other
 import os
 import math
 
-lakeAttitash = "Lake Attitash, Amesbury, Essex County, Massachusetts, USA"
-lakeShoreline = "Shoreline lake, Mountain View, Santa Clara County, California, United States"
-
-f, axes = plt.subplot_mosaic('AAB;AAC')
-
-cords = regionPolygon(lakeAttitash)
-x = [i[0] for i in cords]
-y = [i[1] for i in cords]
-axes['A'].set_title('MAP')
-axes['A'].fill(x,y,'c')
-axes['A'].axis([min(x), max(x),min(y), max(y)])
-axes['A'].grid()
-
 data_dir = os.path.dirname(__file__) #abs dir
-hull = foil(data_dir+"\\data\\xf-naca001034-il-1000000-Ex.csv", 1, 0.5)
-mod = [[i[0].data()*math.pi/180 for i in hull.liftC],[i[1] for i in hull.liftC]]
-axes['B'].set_title('naca001034 liftC')
-axes['B'].plot(mod[0],mod[1])
-plt.figtext(0, 0.01, '© OpenStreetMap contributors', fontsize = 10)
-plt.show()
+
+class display:
+    def __init__(self,location,boat):
+        self.boat = boat
+
+        #display
+        self.f, self.axes = plt.subplot_mosaic('AAABDD;AAACEE',per_subplot_kw={"B": {"projection": "polar"},},)
+        self.axes['B'].set_title('Sail Forces')
+        self.map(location)
+        self.sailForces()
+        self.controls()
+        #credits
+        plt.figtext(0, 0.01, 'Map: © OpenStreetMap contributors', fontsize = 10)
+        plt.show()
+    def controls(self):
+        self.axes['D'].axis('off')
+        self.windSlider = Slider(
+            ax=self.f.add_axes([2/3+0.1, 0.75, 0.1, 0.03]),
+            label="Wind ",
+            valmin=0,
+            valmax=360,
+            valinit=0,
+        )
+        self.windSlider.on_changed(self.windUpdate)
+    def windUpdate(self,val):
+        self.boat.wind.angle = Angle(1,val)
+        self.sailForces()
+    def map(self,location):
+        cords = regionPolygon(location)
+        x = [i[0] for i in cords]
+        y = [i[1] for i in cords]
+        self.axes['A'].set_title(location.split(" ")[0]+ " map")
+        self.axes['A'].fill(x,y,'c')
+        self.axes['A'].axis([min(x), max(x),min(y), max(y)])
+        plt.minorticks_on()
+        self.axes['A'].grid()
+    def sailForces(self):
+        self.boat.update()
+        self.axes['B'].cla()
+        #self.sailf.set_ydata([0,self.boat.forces['sails'].norm])
+        #self.sailf.set_xdata([Angle.norm(self.boat.forces['sails'].angle).calc()*math.pi/180]*2)
+        self.axes['B'].plot([Angle.norm(self.boat.forces['sails'].angle).calc()*math.pi/180]*2,[0,self.boat.forces['sails'].norm],marker= "o")
+
+
+if __name__ == "__main__":
+    lakeAttitash = "Lake Attitash, Amesbury, Essex County, Massachusetts, USA"
+    lakeShoreline = "Shoreline lake, Mountain View, Santa Clara County, California, United States"
+
+    hull = foil(data_dir+"\\data\\xf-naca001034-il-1000000-Ex.csv", 1, 0.5)
+    sail = foil(data_dir+"\\data\\mainSailCoeffs.cvs", 0.128, 1)
+    wind = Vector(Angle(1,225),10) # Going South wind, 10 m/s
+    boat = Boat([hull],[sail],wind)
+    render = display(lakeAttitash,boat)

e0f1b38f81d25d591122bcbe36f255b6b75daff5
henri849 <hsayag2024@kehillahstudent.org>
2023-03-23 23:32:28 -0700

    added mass, linear and rotational velocity, corresponding update functions.

--- Boat.py
+++ Boat.py
@@ -2,28 +2,41 @@ import math
 from Variables import *
 
 class Boat:
-    def __init__(self, hulls, sails, wind, hull_placement = 0):
+    def __init__(self, hulls, sails, wind,mass =10):
         self.hulls = hulls #array of hulls
         self.sails = sails
         self.wind = wind
+        self.mass = mass # mass in in kg
         self.angle = Angle(1,90) # global rotation of boat and all it's parts
         #Forces on the boat
         self.forces = {"sails":Vector(Angle(1,90),0), "hulls":Vector(Angle(1,90),0)}
 
-        #current boat velocity
-        self.velocity = Vector(Angle(1,90),0)
+        #current boat velocities
+        self.linearVelocity = Vector(Angle(1,90),0) # m/s
+        self.rotationalVelocity = 0 # radians/s, positive is ccw, negative is cw
 
         #current boat position
         self.position = Vector(Angle(1,90),0)
+
     def setPos(self,pos):
         self.position = pos
-    def update(self,t=1): #t is in seconds
+    def update(self,dt=1): #t is in seconds
         #update forces
         self.updateSailForces()
         self.updateHullForces()
         #update velocities
-
+        self.updateLinearVelocity(dt)
+        self.updateRotationalVelocity(dt)
         #update position
+
+    def updateLinearVelocity(self,dt):
+        #not using extend is on purpose, extend modifies the variable
+        ax = (sum(self.forces["sails"])+sum(self.forces["hulls"])).xcomp()/self.mass
+        ay = (sum(self.forces["sails"])+sum(self.forces["hulls"])).ycomp()/self.mass
+        a = Vector(Angle(1,round(math.atan2(ay,ax)*180/math.pi*10000)/10000),math.sqrt(ax**2+ay**2))
+        a *= dt
+        self.linearVelocity += a
+    def updateRotationalVelocity(self,t):
         pass
 
     def updateSailForces(self):
@@ -63,7 +76,7 @@ class Boat:
 
     def globalAparentWind(self):
         # returns global aparent wind on boat
-        return self.wind-self.velocity
+        return self.wind-self.linearVelocity
     
     def sailAparentWind(self,idx=0):
         # returns local aparent wind on boat (ie: wind angle in perspective of given sail)

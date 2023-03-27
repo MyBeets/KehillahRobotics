49fe205e4f43053aa49cf6824785c1d536235670
henri849 <hsayag2024@kehillahstudent.org>
2023-03-26 19:28:17 -0700

    Displayed connections between hulls and implemented sail angle displaying.

--- Display.py
+++ Display.py
@@ -18,6 +18,8 @@ class boatDisplayShell():
         self.ax = ax
         self.boat = Boat
     def show(self):
+        cx = -1
+        cy = -1
         for h in self.boat.hulls:
             verts = [(self.meter2degree(i[0]*h.size +h.position.xcomp())+self.boat.position.xcomp(),self.meter2degree(i[1]*h.size+h.position.ycomp())+self.boat.position.ycomp()) for i in h.polygon]
             polygon = patches.Polygon(verts, color="red") 
@@ -25,17 +27,23 @@ class boatDisplayShell():
             r = transforms.Affine2D().rotate_deg_around(self.boat.position.xcomp()+self.meter2degree(h.position.xcomp()),self.boat.position.ycomp()+self.meter2degree(h.position.ycomp()),(self.boat.angle+h.angle).display())
             polygon.set_transform(r+ self.ax.transData)
             self.ax.add_patch(polygon)
+            if len(self.boat.hulls) > 1:
+                if cx == -1:
+                    cx = self.boat.position.xcomp()+self.meter2degree(h.position.xcomp())
+                    cy = self.boat.position.ycomp()+self.meter2degree(h.position.ycomp())
+                else:
+                    self.ax.plot([cx,self.boat.position.xcomp()+self.meter2degree(h.position.xcomp())],[cy,self.boat.position.ycomp()+self.meter2degree(h.position.ycomp())], color = 'gray')
+                    cx = self.boat.position.xcomp()+self.meter2degree(h.position.xcomp())
+                    cy = self.boat.position.ycomp()+self.meter2degree(h.position.ycomp())
 
         for s in self.boat.sails:
             #x1, y1 are the points on the mast
-            x1 = self.boat.position.xcomp()+self.meter2degree(math.cos((boat.angle.calc()+s.position.angle.calc())*math.pi/180)*s.position.norm)
-            y1 = self.boat.position.ycomp()+self.meter2degree(math.sin((boat.angle.calc()+s.position.angle.calc())*math.pi/180)*s.position.norm)
-            print(math.sin(boat.angle.calc()+s.position.angle.calc()*math.pi/180),math.cos(boat.angle.calc()+s.position.angle.calc()*math.pi/180))
-            print(x1,y1,self.boat.position.ycomp())
-            self.ax.plot([x1,self.boat.position.xcomp()],[y1,self.boat.position.ycomp()], color = 'yellow')
-            self.ax.plot([self.boat.position.xcomp()],[self.boat.position.ycomp()], color = 'pink', marker="o", markersize=5)
-            #self.ax.plot(self.boat.position.xcomp(),self.boat.position.ycomp(), color = 'g')
-            #verts = [(x1,y1),()]
+            x1 = self.boat.position.xcomp()+self.meter2degree(math.cos((boat.angle.calc()+s.position.angle.calc()-90)*math.pi/180)*s.position.norm)
+            y1 = self.boat.position.ycomp()+self.meter2degree(math.sin((boat.angle.calc()+s.position.angle.calc()-90)*math.pi/180)*s.position.norm)
+            x2 = x1+self.meter2degree(math.cos((self.boat.angle.display()+s.angle.display())*math.pi/180)*s.size)
+            y2 = y1+self.meter2degree(math.sin((self.boat.angle.display()+s.angle.display())*math.pi/180)*s.size)
+            self.ax.plot([x1,x2],[y1,y2], color = 'yellow')
+            #self.ax.plot([x1],[y1], color = 'pink') #mast or something
 
 
     def meter2degree(self, v):
@@ -71,7 +79,7 @@ if __name__ == "__main__":
     vaka = foil(data_dir+"\\data\\xf-naca001034-il-1000000-Ex.csv", 1, 0.5,rotInertia = 1,size = 1.8)
     ama1 = foil(data_dir+"\\data\\naca0009-R0.69e6-F180.csv", 1, 0.5,position = Vector(Angle(1,0),0.6),rotInertia = 1,size = 1.5)
     ama2 = foil(data_dir+"\\data\\naca0009-R0.69e6-F180.csv", 1, 0.5,position = Vector(Angle(1,180),0.6),rotInertia = 1,size = 1.5)
-    sail = foil(data_dir+"\\data\\mainSailCoeffs.cvs", 0.128, 1, position = Vector(Angle(1,90),0.4),rotInertia = 1,size = 1.2)
+    sail = foil(data_dir+"\\data\\mainSailCoeffs.cvs", 0.128, 1, position = Vector(Angle(1,90),0.4),rotInertia = 1,size = 0.7)
     wind = Vector(Angle(1,225),10) # Going South wind, 10 m/s
     boat = Boat([ama1,vaka,ama2],[sail],wind)
     xpos = -122.09064

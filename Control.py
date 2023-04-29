c192165995e5745c9c31d122dafc3e23d1d97480
henri849 <hsayag2024@kehillahstudent.org>
2023-04-28 17:31:08 -0700

    Fixed noise control, separated sail and rudder autocontrol into functions

--- Control.py
+++ Control.py
@@ -13,7 +13,10 @@ class Controler():
     def __init__(self,Boat, waypoint):
         self.boat = Boat
         self.waypoint = waypoint
-    def update(self,dt,rNoise= 40,stability=1): # less noise = faster rotation, stability tries to limit angular momentum
+    def update(self,dt,rNoise= 2,stability=1): # less noise = faster rotation, stability tries to limit angular momentum
+        self.updateRudder(rNoise,stability)
+        self.updateSails()
+    def updateRudder(self,rNoise,stability):
         dx = self.waypoint[0]-self.boat.position.xcomp()
         dy = self.waypoint[1]-self.boat.position.ycomp()
         target_angle = Angle(1,math.atan2(dy,dx)*180/math.pi)
@@ -22,12 +25,10 @@ class Controler():
         rotV = self.boat.rotationalVelocity*180/math.pi *0.03
         # coeff = 1-(1/(dtheta.calc()*(1/rotV)+1))
         dtheta = printA(dtheta)
-        coeff = math.atan((dtheta)/rNoise) - rotV/stability
-        self.boat.hulls[-1].angle = Angle(1,-10*coeff)*2
-
-        self.boat.sails[0].angle = (Angle(1,45) - ((self.boat.wind.angle+Angle(1,180)) - self.boat.angle))*-1
-        # if dtheta.calc() > 0:
-        #     self.boat.hulls[-1].angle = self.boat.hulls[-1].angle*-1
-        print(dtheta,target_angle,coeff,-10*coeff,self.boat.hulls[-1].angle)
-        
-        #self.boat.hulls[-1].angle += dtheta*dt
+        coeff = math.atan((dtheta)/40) - rotV/stability
+        self.boat.hulls[-1].angle = Angle(1,-10*coeff)*rNoise
+        #print(dtheta,target_angle,coeff,-10*coeff,self.boat.hulls[-1].angle)
+    
+    def updateSails(self):
+        print(Angle.norm(self.boat.globalAparentWind().angle+Angle(1,180)))
+        self.boat.sails[0].angle = (self.boat.globalAparentWind().angle+Angle(1,180)-Angle(1,45))

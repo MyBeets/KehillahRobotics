0beeb346c304d2affd1748dd61291947dafa0de1
henri849 <hsayag2024@kehillahstudent.org>
2023-04-29 19:47:44 -0700

    Changed to new optimal angles obtained from compressor, also started to integrate competition specific navs

--- Control.py
+++ Control.py
@@ -13,22 +13,42 @@ class Controler():
     def __init__(self,Boat, waypoint):
         self.boat = Boat
         self.waypoint = waypoint
+        self.target_angle = Angle(1,0)
+    def plan(self,plantype,waypoints):
+        #type can either E(ndurance), S(tation Keeping), Pr(ecision Navigation), P(ayload),
+        plantype = plantype.lower()
+        if plantype == "e":
+            # Format of waypoints is as such
+            # 4 Buoy in order of navigation
+            pass
+        elif plantype == "s":
+            #4 Buoy in any order
+            pass
+        elif plantype == "Pr":
+            # 4 Buoy in order of navigation
+            pass
+    def setTarget(self,angle):
+        self.target_angle = angle
+
     def update(self,dt,rNoise= 2,stability=1): # less noise = faster rotation, stability tries to limit angular momentum
         self.updateRudder(rNoise,stability)
         self.updateSails()
     def updateRudder(self,rNoise,stability):
-        dx = self.waypoint[0]-self.boat.position.xcomp()
-        dy = self.waypoint[1]-self.boat.position.ycomp()
-        target_angle = Angle(1,math.atan2(dy,dx)*180/math.pi)
+        # dx = self.waypoint[0]-self.boat.position.xcomp()
+        # dy = self.waypoint[1]-self.boat.position.ycomp()
+        # target_angle = Angle(1,math.atan2(dy,dx)*180/math.pi)
+        # target_angle = angle
         current_angle = self.boat.linearVelocity.angle
-        dtheta = (target_angle - current_angle).calc()
+        dtheta = (self.target_angle - current_angle).calc()
         rotV = self.boat.rotationalVelocity*180/math.pi *0.03
         # coeff = 1-(1/(dtheta.calc()*(1/rotV)+1))
         dtheta = printA(dtheta)
-        coeff = math.atan((dtheta)/40) - rotV/stability
+        coeff = 2/math.pi * math.atan((dtheta)/40 - rotV/stability)
         self.boat.hulls[-1].angle = Angle(1,-10*coeff)*rNoise
-        #print(dtheta,target_angle,coeff,-10*coeff,self.boat.hulls[-1].angle)
+        #print(dtheta,self.target_angle,coeff,-10*coeff,self.boat.hulls[-1].angle)
+        print(Angle.norm(self.boat.globalAparentWind().angle+Angle(1,180)))
     
     def updateSails(self):
-        print(Angle.norm(self.boat.globalAparentWind().angle+Angle(1,180)))
-        self.boat.sails[0].angle = (self.boat.globalAparentWind().angle+Angle(1,180)-Angle(1,45))
+        #print(Angle.norm(self.boat.globalAparentWind().angle+Angle(1,180)))
+        # self.boat.sails[0].angle = (self.boat.globalAparentWind().angle+Angle(1,180)-Angle(1,38))
+        self.boat.sails[0].angle = (self.boat.globalAparentWind().angle+Angle(1,180)-Angle(1,60))

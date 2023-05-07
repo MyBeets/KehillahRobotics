70008c677d8eb5661743ce9ffa198f6e9f794fa7
henri849 <hsayag2024@kehillahstudent.org>
2023-05-06 21:43:21 -0700

    Started framework for speed polars

--- Compressor.py
+++ Compressor.py
@@ -9,43 +9,69 @@ def roundNum(x):
 def generateSailPolars(boat,filename):
     boat = copy.deepcopy(boat)
     boat.rotationalVelocity = 0
-    for aoa in range(0,180):
-        #Set wind
-        boat.wind = Vector(Angle(1,180),1)
-        #Boat going in a direction
-        boat.angle = Angle(1,30)
+    data = {}
+    for baoa in range(-90,90):
+    #if True:
+        #baoa = 0
+        mAngle = 0
+        mValue = 0
+        for aoa in range(0,90-baoa):
+            #Set wind
+            boat.wind = Vector(Angle(1,270),1)
+            #Boat going in a direction
+            boat.angle = Angle(1,baoa)
+            boat.sails[0].angle = Angle(1,aoa)
 
-        boat.sails[0].angle = Angle(1,aoa)
-        boat.updateSailForcesandMoments(1)
+            # boat.sails[0].angle = Angle(1,aoa)
+            #F = boat.sails[0].liftForce(boat.sailAparentWind(0)).xcomp()
+            boat.updateSailForcesandMoments(1)
+            boat.updateHullForcesandMoments()
+            F = boat.forces["sails"]#+boat.forces["hulls"]
+            #Now we compute how much it contributes to thrust by a simple dot product
+            F = abs(F * Vector(boat.angle,F.norm))*math.cos(aoa*math.pi/180)
+            #print(F)
+            # F= abs(boat.forces["sails"].norm)
+            if F > mValue:
+                mValue = F
+                mAngle = aoa
+            #print("(",boat.forces["sails"].xcomp(),",",boat.forces["sails"].ycomp(),")")
+            #print("(",math.cos(aoa*math.pi/180)*F,",",math.sin(aoa*math.pi/180)*F,")")
+        data[str(baoa)]=mAngle
+        print("(",baoa,",",mAngle,")")
+        #print("(",math.cos(baoa*math.pi/180)*mAngle,",",math.sin(baoa*math.pi/180)*mAngle,")")
+    print(data)
 
-        F= abs(boat.forces["sails"].xcomp())
-        print("(",math.cos(aoa*math.pi/180)*F,",",math.sin(aoa*math.pi/180)*F,")")
 
 def generatePolars(boat,filename):
     generateSailPolars(boat,filename)
     return
     boat = copy.deepcopy(boat)
     boat.rotationalVelocity = 0
-    for aoa in range(0,-180,-1):
+    for aoa in range(-90,90):
         #Set wind
         boat.wind = Vector(Angle(1,180),1)
         #Boat going in a direction
         boat.angle = Angle(1,aoa)
         # boat.linearVelocity = Vector(Angle(1,aoa),1)
 
-        #We then set optimal sail configuration
-        # boat.sails[0].angle = Angle(1,38)
-        boat.updateSailForcesandMoments()
-        # boat.updateHullForcesandMoments()
-        # F= abs((boat.forces["sails"]+boat.forces["hulls"]).xcomp())4
+        for s in range(30):
+            for ms in range(100): # this must be kept high as to avoid over amplifying innacuracy loops 
+                # boat.sails[0].angle = Angle(1,38)
+                #We then set optimal sail configuration
+                boat.sails[0].setSailRotation(boat.globalAparentWind().angle+Angle(1,180)-Angle(1,38))
+                num =2
+                time = 0.01
+                #update velocities
+                for i in range(num):
+                    boat.updateLinearVelocity(time/num)
+                    boat.updateRotationalVelocity(time/num)
+                    boat.updateSailForcesandMoments(time/num)
+                    boat.updateHullForcesandMoments()
+                # boat.updatePosition(time)
+                # F= abs((boat.forces["sails"]+boat.forces["hulls"]).xcomp())4
         F= abs((boat.forces["sails"]).xcomp())
-        #print(boat.forces["sails"])
+        # print("(",s,",",F,")")
         print("(",math.cos(aoa*math.pi/180)*F,",",math.sin(aoa*math.pi/180)*F,")")
-        # for sAOA in range(0,90):
-        #     boat.sails[0].angle = Angle(1,sAOA)
-        #     boat.updateSailForcesandMoments()
-        #     print("(",sAOA,",",boat.forces["sails"].norm,")")
-        # break
 
 
 

be0619ea6b430a8310655fdfb1bc01854624876d
henri849 <hsayag2024@kehillahstudent.org>
2023-05-13 14:57:20 -0700

    Minor fixes such as rounding and correctly placing loading bar

--- Compressor.py
+++ Compressor.py
@@ -1,9 +1,6 @@
-from Foil import foil
 from Variables import *
-# from Boat import Boat
-import copy
-import math
 from Control import *
+import copy
 from tqdm import tqdm
 
 def roundNum(x):
@@ -17,13 +14,12 @@ def printA(x):
 def generatePolars(boat,filename):
     boat = copy.deepcopy(boat)
     cont = Controler(boat,[])
-    # speeds = [0.3,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5,5.5,6,6.5,7] #.58 kt to 13.6kt
-    speeds = [1] #.58 kt to 13.6kt
+    speeds = [0.3,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5,5.5,6,6.5,7] #.58 kt to 13.6kt
     output = open(filename + ".pol","w")
     output.write("twa/tws;"+str(speeds).replace(", ", ";")[1:-1] + "\n")
-    for aoa in range(-90,90,15):
+    for aoa in range(-90,91,10):
         comp = []
-        for s in speeds:
+        for s in tqdm(speeds, desc="Computing "+str(90-aoa)+"..."):
             #clean slate
             boat.resetValues()
             #Set wind
@@ -34,7 +30,7 @@ def generatePolars(boat,filename):
 
             num =10
             time = 0.01
-            for s in tqdm(range(30), desc="Computing "+str(90-aoa)+"..."):
+            for s in range(30):
                 for ms in range(100): # this must be kept high as to avoid over amplifying innacuracy loops 
                     #We then set optimal sail configuration and all
                     cont.update(time)
@@ -47,7 +43,7 @@ def generatePolars(boat,filename):
             F = boat.linearVelocity#boat.forces["sails"]#.norm #+boat.forces["hulls"]
             #print("(",aoa,",",F,")")
             F = abs(F * Vector(boat.angle,F.norm))#*math.cos(aoa*math.pi/180)
-            comp.append(F)
+            comp.append(round(F*100000)/100000)
             #print("(",math.cos(aoa*math.pi/180)*F,",",math.sin(aoa*math.pi/180)*F,")")
         output.write(str(90-aoa) + ";"+str(comp).replace(", ", ";")[1:-1] + "\n")
     output.close()
@@ -57,34 +53,5 @@ def generatePolars(boat,filename):
 
 
 def Compressor(boat,filename):
-    boat = copy.deepcopy(boat)
-    speeds = 1
-    #file = open(filename,"w+")
-    sails = []
-    hulls = []
-    rudder= []
-    for aoa in range(0,180):
-        hullForces = Vector(Angle(1,0),0)
-        sailForces = Vector(Angle(1,0),0)
-        boat.rotationalVelocity = 0
-        boat.angle = Angle(1,0)
-        boat.linearVelocity = Vector(Angle(1,180+aoa),1)
-        for idx in range(len(boat.hulls)-1):
-            force = boat.hullLiftForce(idx) + boat.hullDragForce(idx)
-            hullForces += force
-        
-        rudderForce = boat.hullLiftForce(len(boat.hulls)-1) + boat.hullDragForce(len(boat.hulls)-1)
-        
-        boat.linearVelocity = Vector(Angle(1,0),0)
-        boat.wind = Vector(Angle(1,180+aoa),1)
-        for idx, s in enumerate(boat.sails):
-            s.angle = Angle(1,0)
-            force = boat.sailLiftForce(idx) + boat.sailDragForce(idx)
-            sailForces += force
-
-        hulls.append("(" +str(aoa)+ ","+str(roundNum(hullForces.xcomp()))+")")
-        sails.append("(" +str(aoa)+ ","+str(roundNum(sailForces.xcomp()))+")")
-        rudder.append("(" +str(aoa)+ ","+str(roundNum(rudderForce.ycomp()-rudderForce.xcomp()*10))+")")
-        # hulls.append("(" + str(roundNum(hullForces.norm))+ ","+str(roundNum(hullForces.angle.calc()))+")")
-    # print(str(hulls).replace("'",""))
-    #print(str(rudder).replace("'",""))
+    # here we generate polars and find stuff like best VMG
+    pass
